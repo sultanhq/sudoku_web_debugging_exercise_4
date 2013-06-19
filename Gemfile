0988d23843ae12682e0dea36a2a21e8e081fee83
Evgeny Shadchnev <evgeny.shadchnev@gmail.com>
2013-06-19 10:09:28 +0100

    added config.ru and wrapped controller code in a class

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ gem 'sinatra'
 gem 'sinatra-flash'
 
 group :development do
-	gem 'shotgun'
-	gem "test-unit"
-	gem 'debugger'
+  gem 'shotgun'
+  gem "test-unit"
+  gem 'debugger'
 end
--- game.rb
+++ game.rb
@@ -7,28 +7,32 @@ configure do
   enable :sessions
 end
 
-post '/' do
-  cell_values = params[:cells] # extract the cell values from the params hash
-  puzzle_string = cell_values.map {|x| x == "" ? "0" : x}.join # convert to a string
-  sudoku_puzzle = Sudoku.new(puzzle_string) # create new instance of model class
-  flash[:info] = sudoku_puzzle.solved? ? "You won" : "You lost"
-  @cells = sudoku_puzzle.cells
-  # session[:sudoku_string] = sudoku_puzzle.to_s
-  redirect to('/')
-end
+class SudokuWeb < Sinatra::Application
 
-get '/' do
-  if session[:sudoku_string]
-    sudoku_puzzle = Sudoku.new(session[:sudoku_string])
-  else
-    sudoku_puzzle = Sudoku.generate
-    session[:sudoku_string] = sudoku_puzzle.to_s
+  post '/' do
+    cell_values = params[:cells] # extract the cell values from the params hash
+    puzzle_string = cell_values.map {|x| x == "" ? "0" : x}.join # convert to a string
+    sudoku_puzzle = Sudoku.new(puzzle_string) # create new instance of model class
+    flash[:info] = sudoku_puzzle.solved? ? "You won" : "You lost"
+    @cells = sudoku_puzzle.cells
+    # session[:sudoku_string] = sudoku_puzzle.to_s
+    redirect to('/')
   end
-  @cells = sudoku_puzzle.cells
-  erb :home
-end
 
-get '/new' do
-  session.clear
-  redirect to('/')
+  get '/' do
+    if session[:sudoku_string]
+      sudoku_puzzle = Sudoku.new(session[:sudoku_string])
+    else
+      sudoku_puzzle = Sudoku.generate
+      session[:sudoku_string] = sudoku_puzzle.to_s
+    end
+    @cells = sudoku_puzzle.cells
+    erb :home
+  end
+
+  get '/new' do
+    session.clear
+    redirect to('/')
+  end
+
 end
